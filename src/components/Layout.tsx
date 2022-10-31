68fefe32030befdbfa1571a1e1c9d07cc10914f4
Grayton Ward <graytonio.ward@gmail.com>
2022-10-31 18:26:53 -0400

    Fixed up styling for mobile

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,14 +6,14 @@ type LayoutProps = {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <main className="mx-auto flex h-screen min-h-screen flex-col bg-[#181818] p-2">
-      <div className="mb-2 md:flex md:items-center md:justify-between">
+    <main className="mx-auto flex h-screen min-h-screen max-w-7xl flex-col bg-[#181818] p-2">
+      <div className="mb-2 flex items-center justify-between">
         <div className="min-w-0 flex-1">
           <h2 className="text-2xl font-bold leading-7 text-gray-100 sm:truncate sm:text-3xl sm:tracking-tight">
             MyStream
           </h2>
         </div>
-        <div className="mt-4 flex md:mt-0 md:ml-4">
+        <div className="flex">
           <AuthButton />
         </div>
       </div>
